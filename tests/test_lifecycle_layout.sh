0f1d5a6f37a44dad7241adb87cec5ed09490cf4f
Vincent Janelle <randomfrequency@gmail.com>
2026-04-19 11:22:49 -0700

    test: make lifecycle layout test self-contained

--- tests/test_lifecycle_layout.sh
+++ tests/test_lifecycle_layout.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+cd "$(dirname "$0")/.."
+
 for path in \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/run \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/type \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/dependencies.d/base \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-core/run \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-core/type \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-core/dependencies.d/postgres-pre \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/run \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/type \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/dependencies.d/postgres-core \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/type \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-pre \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-core \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-post \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/postgres \
-  postgresql18-addon/rootfs/usr/local/bin/provisioning-entrypoint
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/run \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/type \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/dependencies.d/base \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-core/run \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-core/type \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-core/dependencies.d/postgres-pre \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-post/run \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-post/type \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres-post/dependencies.d/postgres-core \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres/type \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-pre \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-core \
+  rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-post \
+  rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/postgres \
+  rootfs/usr/local/bin/provisioning-entrypoint
 do
   test -f "$path"
 done
