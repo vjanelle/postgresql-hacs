c8a7377f20db2c3d245e22c7a064edd206e6fcc2
Vincent Janelle <randomfrequency@gmail.com>
2026-04-19 12:04:08 -0700

    fix: preserve postgres grant privileges

--- rootfs/usr/share/postgresql/bootstrap/databases.sql
+++ rootfs/usr/share/postgresql/bootstrap/databases.sql
@@ -1,30 +1,13 @@
-\set ON_ERROR_STOP on
-
-SELECT format(
-  'CREATE DATABASE %I%s',
-  :'db_name',
-  CASE
-    WHEN NULLIF(:'db_owner', '') IS NULL THEN ''
-    ELSE format(' OWNER %I', :'db_owner')
-  END
-)
-WHERE NOT EXISTS (
-  SELECT 1
-  FROM pg_database
-  WHERE datname = :'db_name'
-)
-\gexec
-
-SELECT format(
-  'ALTER DATABASE %I OWNER TO %I',
-  :'db_name',
-  :'db_owner'
-)
-WHERE NULLIF(:'db_owner', '') IS NOT NULL
-  AND EXISTS (
-    SELECT 1
-    FROM pg_database
-    WHERE datname = :'db_name'
-      AND pg_get_userbyid(datdba) IS DISTINCT FROM :'db_owner'
-  )
-\gexec
+DO $$
+BEGIN
+  IF NULLIF(:'db_owner', '') IS NOT NULL
+    AND EXISTS (
+      SELECT 1
+      FROM pg_database
+      WHERE datname = :'db_name'
+        AND pg_get_userbyid(datdba) IS DISTINCT FROM :'db_owner'
+    ) THEN
+    EXECUTE format('ALTER DATABASE %I OWNER TO %I', :'db_name', :'db_owner');
+  END IF;
+END
+$$;
