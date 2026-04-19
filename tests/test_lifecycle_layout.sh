223d55f5c9be16acd66d6d92a075262baf32403d
Vincent Janelle <randomfrequency@gmail.com>
2026-04-19 11:14:12 -0700

    feat: fix postgresql18 s6 lifecycle wiring

--- tests/test_lifecycle_layout.sh
+++ tests/test_lifecycle_layout.sh
@@ -3,8 +3,19 @@ set -euo pipefail
 
 for path in \
   postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/run \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/type \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-pre/dependencies.d/base \
   postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-core/run \
-  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/run
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-core/type \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-core/dependencies.d/postgres-pre \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/run \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/type \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres-post/dependencies.d/postgres-core \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/type \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-pre \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-core \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/postgres/contents.d/postgres-post \
+  postgresql18-addon/rootfs/etc/s6-overlay/s6-rc.d/user/contents.d/postgres
 do
   test -f "$path"
 done
